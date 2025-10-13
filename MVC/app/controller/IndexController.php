bda707666508a37faadb0640b9dd45fc2cf35aa7
CesarG503 <cesargaray06@gmail.com>
2025-10-13 10:43:51 -0600

    feat: enrutamiento iniciar y coneccion de base de datos

---
+++ MVC/app/controller/IndexController.php
@@ -0,0 +1,27 @@
+<?php 
+namespace app\controller;
+
+class IndexController{
+
+
+    public function Index(){
+        return $this->view("index",["title"=>'INCIO']);
+    }
+    public function view($view, $data = []){
+        extract($data);
+        if(file_exists("../app/views/$view.php"))
+        {
+            ob_start();
+            include("../app/views/$view.php");
+            $content = ob_get_clean();
+            return $content;
+        }
+        else
+            {
+                echo "No se encontro la vista";
+            }
+    }
+
+}
+
+?>
---
+++ MVC/lib/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace lib;;
+use PDO;
+
+class Database{
+
+    public $host = "db";
+    public $dbname = "estudiantes";
+    public $username = "root";
+    public $password = "rootpass";
+
+    public $conn;
+
+    public function getConnetion(){
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname , $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (\Throwable $th) {
+            echo $th;
+        }
+        return $this->conn;
+    }
+}
+
+?>
