ef78c023c65a3011c05ec227cf7854105e61c942
CesarG503 <cesargaray06@gmail.com>
2025-10-13 21:47:56 -0600

    feat: las rutas de los dias y del perfil

--- MVC/app/controller/IndexController.php
+++ MVC/app/controller/IndexController.php
@@ -7,6 +7,18 @@ class IndexController{
     public function Index(){
         return $this->view("index",["title"=>'INCIO']);
     }
+    public function Dia1(){
+        return $this->view("dia1", ["title" => 'Día 1 - Semana de Sistemas']);
+    }
+
+    public function Dia2(){
+        return $this->view("dia2", ["title" => 'Día 2 - Semana de Sistemas']);
+    }
+
+    public function MiInformacion(){
+        return $this->view("mi-informacion", ["title" => 'Mi Información']);
+    }
+
     public function view($view, $data = []){
         extract($data);
         if(file_exists("../app/views/$view.php"))
--- MVC/routes/web.php
+++ MVC/routes/web.php
@@ -4,6 +4,9 @@ use lib\Route;
 use app\controller\IndexController;
 
 Route::get("/",[IndexController::class,"Index"]);
+Route::get("/dia1",[IndexController::class,"Dia1"]);
+Route::get("/dia2",[IndexController::class,"Dia2"]);
+Route::get("/yo",[IndexController::class,"MiInformacion"]);
 
 Route::dispatch();
 
